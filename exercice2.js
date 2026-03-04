91a7caba9793d2f3fe7a16f28c18362cc3135425
Aissam <a.midine7420@uca.ac.ma>
2026-03-04 15:28:29 +0000

    Add 8 javascript exercices

---
+++ exercice2.js
@@ -0,0 +1,15 @@
+let valeurs = [0, 1, "", "0", null, undefined, NaN, false, [], {}];
+
+for (let i = 0; i < valeurs.length; i++) {
+    let valeur = valeurs[i];
+
+    let affichage = String(valeur);
+
+    if (valeur === "") {
+        affichage = "(chaine vide)";
+    }
+
+    let etat = valeur ? "truthy" : "falsy";
+
+    console.log(affichage + " -> " + etat);
+}
